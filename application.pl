abf21c585cc93f8c12756839e9b1a9f72adb66b0
swistaq <xswistaqx@gmail.com>
2016-11-13 17:31:28 +0100

    Dodanie pytania o preferencje sposobu uczenia; dodane wstepnie wpisy dla innych jezykow.

--- application.pl
+++ application.pl
@@ -1,12 +1,49 @@
 % hypothesis
 language(python) :-
-  why(entertainment).
+  why(entertainment),
+  learning_preference(easy).
 
 language(java) :-
-  why(money).
+  why(money),
+  learning_preference(easy).
 
+language(cpp) :-
+  learning_preference(hard).
+
+language(c) :-
+  learning_preference(hardest).
+
+language(objc) :-
+  why(money),
+  learning_preference(easy).
+  
+/*
+language(js) :-
+
+
+
+
+language(php) :-
+  
+
+language(ruby) :-
+*/
+
+language(csharp) :-	
+  why(money),
+  learning_preference(easy).
+ 
 question(why) :-
   write('Dlaczego chcesz nauczyc sie programowac?'), nl.
+  
+question(learning_preference) :-
+  write('Jakich rzeczy wolisz sie uczyc?'), nl.
+
+learning_preference(Answer) :-
+  knowledge_base(learning_preference, Answer).
+learning_preference(Answer) :-
+  \+ knowledge_base(learning_preference, _),
+  ask(learning_preference, Answer, [easy, hard, hardest]).  
 
 why(Answer) :-
   knowledge_base(why, Answer).
@@ -14,6 +51,15 @@ why(Answer) :-
   \+ knowledge_base(why, _),
   ask(why, Answer, [entertainment, money]).
 
+answer(easy) :-
+  write('latwych').
+
+answer(hard) :-
+  write('trudnych').
+  
+answer(hardest) :-
+  write('bardzo trudnych').
+  
 answer(entertainment) :-
   write('Rozrywka').
 
@@ -25,7 +71,29 @@ describe(python) :-
 
 describe(java) :-
   write('Java').
+  
+describe(c) :- 
+  write('C').
+	
+describe(cpp) :- 
+  write('C++').
+  
+describe(objc) :- 
+  write('objective-C').
+  
+describe(php) :- 
+  write('PHP').
 
+describe(ruby) :- 
+  write('Ruby').
+	
+describe(csharp) :- 
+  write('C#').
+  
+describe(js) :- 
+  write('JavaScript').
+
+	
 % utils
 answers([], _).
 answers([Head|Tail], Index) :-
